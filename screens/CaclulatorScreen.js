9c4c362786e66b588ad27dfa92d9f493e53ba04b
AyushiK16 <62651426+AyushiK16@users.noreply.github.com>
2021-08-23 22:07:38 +0530

    Update CaclulatorScreen.js

--- screens/CaclulatorScreen.js
+++ screens/CaclulatorScreen.js
@@ -68,7 +68,7 @@ export default class CalculatorScreen extends React.Component{
                     padding: RFValue(10),
                   }}
                 >
-                    <Text style = {styles.label3}> Do you wish to find out how your performed?</Text>
+                    <Text style = {styles.label3}> Do you wish to know how your performed?</Text>
                 <Text style = {styles.label3}>Enter your marks below and find out!</Text>
                   
                 <Text style={styles.label}>Total Possible Marks for Each Subject </Text>
